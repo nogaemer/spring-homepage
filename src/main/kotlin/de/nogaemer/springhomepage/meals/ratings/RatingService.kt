da3d19faf7d9a8f74d2f5ecb366800591225d2ff
Noah Schwenold <schwenoldnoah@gmail.com>
2024-06-23 17:44:58 +0200

    Rating

--- src/main/kotlin/de/nogaemer/springhomepage/meals/ratings/RatingService.kt
+++ src/main/kotlin/de/nogaemer/springhomepage/meals/ratings/RatingService.kt
@@ -1,16 +1,14 @@
 package de.nogaemer.springhomepage.meals.ratings
 
 import BaseService
-import de.nogaemer.springhomepage.exceptions.AlreadyReported
 import de.nogaemer.springhomepage.exceptions.IdNotFoundException
 import de.nogaemer.springhomepage.exceptions.NotFoundException
 import de.nogaemer.springhomepage.meals.MealRepository
 import de.nogaemer.springhomepage.meals.models.Meal
+import de.nogaemer.springhomepage.meals.ratings.RatingService.RatingUpdateMethod.*
 import de.nogaemer.springhomepage.user.UserRepository
 import de.nogaemer.springhomepage.user.UserResponse
-import okhttp3.internal.wait
 import org.bson.types.ObjectId
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
@@ -24,7 +22,7 @@ class RatingService(
     val mealRepository: MealRepository,
     val userRepository: UserRepository,
     val mongoTemplate: MongoTemplate,
-): BaseService<Rating, ObjectId>(repository, mealRepository, mongoTemplate) {
+) : BaseService<Rating, ObjectId>(repository, mealRepository, mongoTemplate) {
 
     override fun findByUserId(userId: ObjectId): Rating? {
         return repository.findByUserId(userId)
@@ -42,8 +40,7 @@ class RatingService(
         val ratings = mutableListOf<RatingResponse>()
 
         repository.findByMealId(mealId).forEach {
-            val user = userRepository.findById(it.userId)?:
-                throw IdNotFoundException("User not found")
+            val user = userRepository.findById(it.userId) ?: throw IdNotFoundException("User not found")
 
             ratings.add(
                 RatingResponse(
@@ -54,7 +51,8 @@ class RatingService(
                         user.name,
                         user.role
                     )
-                ))
+                )
+            )
         }
         return ratings
     }
@@ -76,25 +74,32 @@ class RatingService(
         val meal = mealRepository.findById(rating.mealId)
             .orElseThrow { throw IdNotFoundException("Meal not found") }
 
-        updateRatings(meal, rating, true)
+        updateRatings(meal, rating, DELETE)
 
         return rating
     }
 
-    fun updateRatings(meal: Meal, rating: Rating, delete: Boolean = false) {
+    fun updateRatings(meal: Meal, rating: Rating, delete: RatingUpdateMethod = ADD, originalRating: Rating? = null) {
         var newAverageRating = 0.0
 
-        if (delete) {
-            if (meal.ratings.size > 1) {
-                newAverageRating = (meal.ratings.sumOf { it.rating } - rating.rating) / (meal.ratings.size - 1).toDouble()
+        when (delete) {
+            ADD -> {
+                newAverageRating =
+                    (meal.ratings.sumOf { it.rating }/ (meal.ratings.size).toDouble())
             }
-        } else {
-            newAverageRating = (meal.ratings.sumOf { it.rating } + rating.rating) / (meal.ratings.size + 1).toDouble()
-        }
 
-        var test = meal.ratings.sumOf { it.rating }
-        test = rating.rating
-        test = meal.ratings.size - 1
+            UPDATE -> {
+                newAverageRating =
+                    (meal.ratings.sumOf { it.rating } - originalRating!!.rating+ rating.rating) / (meal.ratings.size).toDouble()
+            }
+
+            DELETE -> {
+                if (meal.ratings.size > 1) {
+                    newAverageRating =
+                        (meal.ratings.sumOf { it.rating }/ (meal.ratings.size).toDouble())
+                }
+            }
+        }
 
         mongoTemplate.updateMulti(
             Query.query(Criteria.where("id").`is`(rating.mealId)),
@@ -112,13 +117,25 @@ class RatingService(
     }
 
     fun update(id: ObjectId, rating: Rating): Rating? {
-         val originalRating = repository.findById(id).orElseThrow {
-             IdNotFoundException("Meal with id $id not found")
-         }
+        val originalRating = repository.findById(id).orElseThrow {
+            IdNotFoundException("Meal with id $id not found")
+        }
+
+        updateRatings(mealRepository.findById(originalRating.mealId).orElseThrow {
+            NotFoundException("Meal not found")
+        }, rating, UPDATE, originalRating)
 
         originalRating.rating = rating.rating
 
         return repository.save(originalRating)
     }
 
+
+    enum class RatingUpdateMethod {
+        ADD,
+        UPDATE,
+        DELETE
+    }
 }
+
+
